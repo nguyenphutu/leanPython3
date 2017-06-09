fa9a08a5de5106587ab9d57bdc56f9b8a282dbe0
nguyenphutu <nguyentudb1995@gmail.com>
2017-06-10 00:42:25 +0700

    print function and string

---
+++ print_function_and_string.py
@@ -0,0 +1,18 @@
+# print function
+
+print("Hello world")
+print('This is example of print function')
+print("We're devs")
+print('We\'re devs')
+print('He said "Hi!"')
+
+# string
+
+print('Hi ' + 'Tu')
+print('Hi', 'Tu')
+
+# convert
+
+print('Tu ' + str('1995'))
+print(int('5') + 5)
+print(float('5.5') + 5)
