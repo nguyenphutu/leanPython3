6bf2587a71146c92d3e95d8154f398881cc6a07f
nguyenphutu <nguyentudb1995@gmail.com>
2017-06-10 01:08:42 +0700

    if elif and else in python

---
+++ if.py
@@ -0,0 +1,21 @@
+x = 5
+y = 6
+z = 5
+
+if z < y > x:
+    print('y is greater than z va y is greater than x')
+
+if x == z:
+    print('x is equal z')
+
+if y != x:
+    print('y is not equal x')
+
+# if elif else
+
+if x > y :
+    print('x is greater than y')
+elif x==y:
+    print('x is equal y')
+else:
+    print('x is less than y')
