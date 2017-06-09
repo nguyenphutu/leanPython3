05b8d12cf9bd89105fe35c3c66fbfea8abcd2254
nguyenphutu <nguyentudb1995@gmail.com>
2017-06-10 00:43:28 +0700

    variables in python

---
+++ variables.py
@@ -0,0 +1,14 @@
+int_var = 5
+str_var = 'string variables'
+bool_var = True
+
+print(int_var)
+print(str_var)
+print(bool_var)
+
+# gan gia tri cho nhieu bien
+x,y = (5,10)
+# or x,y = 5,10
+
+print(x)
+print(y)
