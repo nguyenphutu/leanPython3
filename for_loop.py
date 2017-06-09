dff8b647a65705580790a07a324d229351fd7ce9
nguyenphutu <nguyentudb1995@gmail.com>
2017-06-10 00:52:05 +0700

    for loop in python 3

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+# in ra cac so tu 1 -> 10
+# range(n) chua tu 1 den n-1
+for i in range(11):
+    # cau lenh
+    print(i)
+
+
+
+# using List
+
+exampleList = [1,2,5,7,4,6,4,5,8,9]
+for eachNumber in exampleList:
+    print(eachNumber)
