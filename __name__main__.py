3ca7e17d38c600b88a9d5342497b655ed4b55594
nguyenphutu <nguyentudb1995@gmail.com>
2017-06-11 17:37:15 +0700

    How to use __name__ == __main__ in python

---
+++ __name__main__.py
@@ -0,0 +1,8 @@
+def examp():
+    print('this is example')
+
+if __name__ == '__main__':
+    print('such great module!!!')
+
+# if __name__ == '__main__':
+# dung de khi import 1 module no k chay luon cac lenh trong module do
