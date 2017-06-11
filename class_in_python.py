2d92607d29cca1551835891b2990e1433c386ee9
nguyenphutu <nguyentudb1995@gmail.com>
2017-06-11 17:36:29 +0700

    Using class in python

---
+++ class_in_python.py
@@ -0,0 +1,18 @@
+class calculator:
+
+    def add(x,y):
+        return x+y
+
+    def sub(x,y):
+        return x-y
+
+    def mul(x,y):
+        return x*y
+
+    def div(x,y):
+        return x/y
+
+# call function of class
+
+added = calculator.add(3,5)
+print(added)
