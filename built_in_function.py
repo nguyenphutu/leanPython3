6b03296e08b593953a329e533e7382e70c8c55e9
nguyenphutu <nguyentudb1995@gmail.com>
2017-06-12 00:30:22 +0700

    use some function in python lib

---
+++ built_in_function.py
@@ -0,0 +1,14 @@
+# -----------------
+exNum1 = -5
+exNum2 = 5
+# tri tuyet doi abs(x)
+print(abs(exNum1))
+# -----------------
+if abs(exNum1) == exNum2:
+    print('There are the same')
+
+x = 5.66666
+# lam tron
+print(round(x))
+
+# another max() min() mean()........
