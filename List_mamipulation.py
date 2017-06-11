f746102f3006a39b0b0c406b44408ebbc7413b31
nguyenphutu <nguyentudb1995@gmail.com>
2017-06-11 23:07:16 +0700

    Thao tac tren list

---
+++ List_mamipulation.py
@@ -0,0 +1,34 @@
+# Thao tac voi List
+
+x = [1,2,3,4,5,6,7,8,9,4]
+
+# # chen vap cuoi danh sach
+# x.append(10)
+# print(x)
+
+# # chen vap vi tri bat ki
+# # chen vao vi tri thu 5 gia tri la 10
+# x.insert(5,10)
+# print(x)
+#
+# # xoa gtri i trong list
+# x.remove(2)
+# print(x)
+# # xoa tai vi tri bat ki
+# x.remove(x[1])
+# print(x)
+
+# # hien thi tu vi tri i -> j
+# print(x[2:5])
+#
+# # hien thi gtri phan tu cuoi cung cua list
+# print(x[-1])
+
+# chi so cua cac phan tu trong list
+print(x.index(2))
+
+# some function in list
+print(x.count(4))
+x.sort()
+print(x)
+
