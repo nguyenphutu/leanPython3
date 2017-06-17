86a850c0aaad1636db1fd5f2f494d1f5b3a32d0e
nguyenphutu <nguyentudb1995@gmail.com>
2017-06-17 19:53:54 +0700

    bai toan fibolacci

---
+++ fibolacci.py
@@ -0,0 +1,26 @@
+# def F():
+#     a,b = 0,1
+#     yield a
+#     yield b
+#     while True:
+#         a, b = b, a + b
+#         yield b
+#
+# def SubFib(startNumber, endNumber):
+#     for cur in F():
+#         if cur > endNumber:
+#             return
+#         if cur >= startNumber:
+#             yield cur
+# for i in SubFib(0, 20):
+#     print(i)
+
+# ---------------
+
+def fib(num):
+    if num <= 1:
+        return num
+    return fib(num-2) + fib(num-1)
+
+for i in range(10):
+    print(i,fib(i))
