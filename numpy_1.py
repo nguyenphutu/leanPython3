0ad3bec8bdaa120a66beb895f78399476f301bb4
nguyenphutu <nguyentudb1995@gmail.com>
2017-06-18 23:21:14 +0700

    array basic in numpy convert list to numpy array

---
+++ numpy_1.py
@@ -0,0 +1,14 @@
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print np_baseball
+print(np_baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
